f83befcfe37f9b9e4a843b20e16dcd7c6b2ffff8
Arina Komissarova <74209845+ArinaMiah@users.noreply.github.com>
2021-02-19 10:21:23 -0500

    readme

---
+++ README.md
@@ -0,0 +1,2 @@
+#This project was created as one of the challanges on FreeCodeCamp
+This is a demonstration of ability to use JavaScript basics
--- js/index.js
+++ js/index.js
@@ -3,24 +3,24 @@ function myFunction() {
   var checkBox = document.getElementById("flower");
   // Get the output text
   var text = document.getElementById("number-label");
-  var secondoption=document.getElementById("if-bouquet");
-  var rose=document.getElementById("flower1");
-   var tulip=document.getElementById("flower2");
-   var lily=document.getElementById("flower3");
+  var secondoption = document.getElementById("if-bouquet");
+  var rose = document.getElementById("flower1");
+  var tulip = document.getElementById("flower2");
+  var lily = document.getElementById("flower3");
 
   // If the checkbox is checked, display the output text
-  if (checkBox.checked == true){
-   text.style.display = "block";
-   secondoption.style.display="none";
-    rose.style.display="block";
-     tulip.style.display="block"; lily.style.display="block";
-    
+  if (checkBox.checked == true) {
+    text.style.display = "block";
+    secondoption.style.display = "none";
+    rose.style.display = "block";
+    tulip.style.display = "block"; lily.style.display = "block";
+
   } else {
     text.style.display = "none";
-    secondoption.style.display="block";
-     rose.style.display="none";
-     tulip.style.display="none"; lily.style.display="none";
-   
+    secondoption.style.display = "block";
+    rose.style.display = "none";
+    tulip.style.display = "none"; lily.style.display = "none";
+
   }
 }
 function myFunction2() {
@@ -28,14 +28,14 @@ function myFunction2() {
   var checkBox = document.getElementById("bouquet");
   // Get the output text
   var text = document.getElementById("do-this");
-    var secondoption=document.getElementById("if-flower");
+  var secondoption = document.getElementById("if-flower");
 
   // If the checkbox is checked, display the output text
-  if (checkBox.checked == true){
+  if (checkBox.checked == true) {
     text.style.display = "block";
-    secondoption.style.display="none";
+    secondoption.style.display = "none";
   } else {
     text.style.display = "none";
-    secondoption.style.display="block";
+    secondoption.style.display = "block";
   }
 }
